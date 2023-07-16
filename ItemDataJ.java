8b2b24fec461a8697f33a85cf532f178b26e551c
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-07-16 15:59:12 -0600

    Update ItemDataJ.java

--- ItemDataJ.java
+++ ItemDataJ.java
@@ -1,11 +1,26 @@
-public class ItemDataJ{
+//PAULA REBECA BARILLAS
 
+public class ItemDataJ{
+    Object originalValue; 
 //public void ItemDataJ(String[] args) {} 
 public ItemDataJ(Object originalValue){
-
-
-
+        this.originalValue = originalValue;
 }
+
+public String getType(){
+    Class class1 =originalValue.getClass();
+    if(class1.equals(String.class)){
+        return "cadena";
+    } else if (class1.equals(Integer.class)){
+        return "entero";
+    } else if (class1.equals(Boolean.class)){
+        return "booleano";
+    } else {
+        return null; 
+    }
+   
+}
+
 
 
 }
